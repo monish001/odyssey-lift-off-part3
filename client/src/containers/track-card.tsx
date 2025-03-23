432498b6b92f4866de41c407e58c0de68dad3770
Monish Gupta - m0g01ts <monish.gupta@walmart.com>
2025-03-23 13:56:54 +0530

    lesson changes

--- client/src/containers/track-card.tsx
+++ client/src/containers/track-card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
+import { Link } from "react-router-dom";
 import type { Track } from "../__generated__/graphql";
 
 /**
@@ -9,10 +10,10 @@ import type { Track } from "../__generated__/graphql";
  * for each track populating the tracks grid homepage.
  */
 const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount } = track;
+  const { title, thumbnail, author, length, modulesCount, id } = track;
 
   return (
-    <CardContainer>
+    <CardContainer to={`/track/${id}`}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail || ""} alt={title} />
@@ -38,7 +39,7 @@ const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
 export default TrackCard;
 
 /** Track Card styled components */
-const CardContainer = styled.div({
+const CardContainer = styled(Link)({
   borderRadius: 6,
   color: colors.text,
   backgroundSize: "cover",
